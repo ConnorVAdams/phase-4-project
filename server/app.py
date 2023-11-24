e0846798de683152a4d560c1203eabd842ac3ad7
Connor Adams <connorvadams@gmail.com>
2023-11-24 13:56:13 -0700

    Adds model validations.

--- server/app.py
+++ server/app.py
@@ -18,6 +18,10 @@ from models import db, Venue, Artist, Concert, Review
 def index():
     return '<h1>Project Server</h1>'
 
+# *************
+# ARTIST ROUTES
+# *************
+
 class Artists(Resource):
 
     def get(self):
@@ -28,6 +32,17 @@ class Artists(Resource):
         ) for artist in Artist.query]
         return artists, 200
     
+    def post(self):
+        try:
+            data = request.get_json()
+            new_artist = Artist(**data)
+            db.session.add(new_artist)
+            db.session.commit()
+            return new_artist.to_dict(), 201
+        except Exception as e:
+            db.session.rollback()
+            return {'error': str(e)}, 400
+    
 api.add_resource(Artists, '/artists')
 
 class ArtistByID(Resource):
@@ -36,13 +51,54 @@ class ArtistByID(Resource):
         if artist := db.session.get(Artist, id):
             return artist.to_dict(
                 rules=(
-                    'concerts.date',
-                    'concerts.time'
                 )
             ), 200
-        return {'error': f'No artist with id {self.id} found.'}
+        return {'error': f'No artist with ID {id} found.'}
+    
+    def patch(self, id):
+        data = request.get_json()
+
+        if artist := db.session.get(Artist, id):
+            try:
+                for attr in data:
+                    setattr(artist, attr, data[attr])
+                db.session.add(artist)
+                db.session.commit()
+                return artist.to_dict(), 202
+            except Exception as e:
+                db.session.rollback()
+                return {'error': str(e)}, 400
+        return {'error': f'No artist with ID {id} found.'}
+    
+    def delete(self, id):
+        if artist := db.session.get(Artist, id):
+            try:
+                db.session.delete(artist)
+                db.session.commit()
+                return '', 204
+            except Exception as e:
+                db.session.rollback()
+                return {'error': str(e)}, 422
+        return {'error': f'No artist with ID {id} found.'}
 
 api.add_resource(ArtistByID, '/artists/<int:id>')
+
+class ConcertsForArtist(Resource):
+
+    def get(self, id):
+        if artist := db.session.get(Artist, id):
+            return [concert.to_dict(
+                rules=(
+                    '-artist',
+                )
+            ) for concert in artist.concerts], 200
+        return {'error': f'No artist with ID {id} found.'}
+
+api.add_resource(ConcertsForArtist, '/artists/<int:id>/concerts')
+
+# ************
+# VENUE ROUTES
+# ************
 
 class Venues(Resource):
 
@@ -53,6 +109,17 @@ class Venues(Resource):
             ) 
         ) for venue in Venue.query]
         return venues, 200
+    
+    def post(self):
+        try:
+            data = request.get_json()
+            new_venue = Artist(**data)
+            db.session.add(new_venue)
+            db.session.commit()
+            return new_venue.to_dict(), 201
+        except Exception as e:
+            db.session.rollback()
+            return {'error': str(e)}, 400
 
 api.add_resource(Venues, '/venues')
 
@@ -65,9 +132,114 @@ class VenueByID(Resource):
 
                 )
             ), 200
-        return {'error': f'No venue with id {self.id} found.'}
+        return {'error': f'No venue with ID {id} found.'}
+    
+    def patch(self, id):
+        data = request.get_json()
+
+        if venue := db.session.get(Venue, id):
+            try:
+                for attr in data:
+                    setattr(venue, attr, data[attr])
+                db.session.add(venue)
+                db.session.commit()
+                return venue.to_dict(), 202
+            except Exception as e:
+                db.session.rollback()
+                return {'error': str(e)}, 400
+        return {'error': f'No venue with ID {id} found.'}
+    
+    def delete(self, id):
+        if venue := db.session.get(Venue, id):
+            try:
+                db.session.delete(venue)
+                db.session.commit()
+                return '', 204
+            except Exception as e:
+                db.session.rollback()
+                return {'error': str(e)}, 422
+        return {'error': f'No venue with ID {id} found.'}
 
 api.add_resource(VenueByID, '/venues/<int:id>')
+
+class ConcertsForVenue(Resource):
+
+    def get(self, id):
+        if venue := db.session.get(Venue, id):
+            return [concert.to_dict(
+                rules=(
+                    '-venue',
+                )
+            ) for concert in venue.concerts], 200
+        return {'error': f'No venue with ID {id} found.'}
+
+api.add_resource(ConcertsForVenue, '/venues/<int:id>/concerts')
+
+# **************
+# CONCERT ROUTES
+# **************
+
+class Concerts(Resource):
+
+    def get(self):
+        concerts = [concert.to_dict(
+            rules=(
+
+            )
+        ) for concert in Concert.query]
+        return concerts, 200
+    
+    def post(self):
+        try:
+            data = request.get_json()
+            new_concert = Concert(**data)
+            db.session.add(new_concert)
+            db.session.commit()
+            return new_concert.to_dict(), 201
+        except Exception as e:
+            db.session.rollback()
+            return {'error': str(e)}, 400
+
+api.add_resource(Concerts, '/concerts')
+
+class ConcertByID(Resource):
+
+    def get(self, id):
+        if concert := db.session.get(Concert, id):
+            return concert.to_dict(
+                rules=(
+
+                )
+            ), 200
+        return {'error': f'No concert with ID {id} found.'}
+    
+    def patch(self, id):
+        data = request.get_json()
+
+        if concert := db.session.get(Concert, id):
+            try:
+                for attr in data:
+                    setattr(concert, attr, data[attr])
+                db.session.add(concert)
+                db.session.commit()
+                return concert.to_dict(), 202
+            except Exception as e:
+                db.session.rollback()
+                return {'error': str(e)}, 400
+        return {'error': f'No concert with ID {id} found.'}
+    
+    def delete(self, id):
+        if concert := db.session.get(Concert, id):
+            try:
+                db.session.delete(concert)
+                db.session.commit()
+                return '', 204
+            except Exception as e:
+                db.session.rollback()
+                return {'error': str(e)}, 422
+        return {'error': f'No concert with ID {id} found.'}
+
+api.add_resource(ConcertByID, '/concerts/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -3,6 +3,8 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
+import re
+from datetime import datetime
 
 from config import db
 
@@ -28,7 +30,40 @@ class Venue(db.Model, SerializerMixin):
         '-created_at',
         '-updated_at'
         )
+    
+    @validates('name')
+    def validates_name(self, _, new_name):
+        if not isinstance(new_name, str):
+            raise TypeError(
+                'Name must be a string.'
+            )
+        elif not len(new_name) in range(1, 31):
+            raise ValueError(
+                'Name must be between 1 and 30 characters.'
+            )
+        return new_name
+    
+    @validates('location')
+    def validates_location(self, _, new_location):
+        approved_neighborhoods = ['Downtown', 'Westside', 'Broadway']
+        if new_location not in approved_neighborhoods:
+            return ValueError(
+                'Location must be one of the following: Downtown, Westside, Broadway'
+            )
+        return new_location
 
+    @validates('link')
+    def validates_link(self, _, new_link):
+        pattern = re.compile(
+            r'^(http|https)://'  # Check for http:// or https:// at the beginning
+            r'([a-zA-Z0-9-.]+(\.[a-zA-Z]{2,})+(/.*)?)$'  # Valid domain and optional path
+        )
+        if not pattern.match(new_link) and new_link != None:
+            raise ValueError(
+                'Link must be a valid URL.'
+            )
+        return new_link
+        
     def __repr__(self):
         return f'<Venue {self.id}: {self.name}>'
 
@@ -51,6 +86,30 @@ class Artist(db.Model, SerializerMixin):
         '-created_at',
         '-updated_at'
         )
+    
+    @validates('name')
+    def validates_name(self, _, new_name):
+        if not isinstance(new_name, str):
+            raise TypeError(
+                'Name must be a string.'
+            )
+        elif not len(new_name) in range(1, 31):
+            raise ValueError(
+                'Name must be between 1 and 30 characters.'
+            )
+        return new_name
+    
+    @validates('genre')
+    def validates_genre(self, _, new_genre):
+        if not isinstance(new_genre, str):
+            raise TypeError(
+                'Genre must be a string.'
+            )
+        elif not len(new_genre) in range(1, 21):
+            raise ValueError(
+                'Genre must be between 1 and 20 characters.'
+            )
+        return new_genre
 
     def __repr__(self):
         return f'<Artist {self.id}: {self.name}>'
@@ -71,6 +130,14 @@ class Concert(db.Model, SerializerMixin):
     artist = db.relationship('Artist', back_populates='concerts')
     venue = db.relationship('Venue', back_populates='concerts')
 
+    @property
+    def date(self):
+        return self.date_time.date().strftime('%m/%d/%Y')
+
+    @property
+    def time(self):
+        return self.date_time.time().strftime('%I:%M %p')
+
     serialize_rules = (
         '-artist.concerts', 
         '-venue.concerts',
@@ -81,15 +148,65 @@ class Concert(db.Model, SerializerMixin):
         '-artist_id',
         '-venue_id',
         '-date_time',
+        'date',
+        'time'
         )
 
-    @property
-    def date(self):
-        return self.date_time.date().strftime('%m/%d/%Y')
-
-    @property
-    def time(self):
-        return self.date_time.time().strftime('%I:%M %p')
+    @validates('date_time')
+    def validates_date_time(self, _, new_date_time):
+        if not isinstance(datetime, new_date_time):
+            raise TypeError(
+                'date_time must be a valid datetime object.'
+            )
+        elif new_date_time < datetime.now():
+            raise ValueError(
+                'date_time must be in the future.'
+            )
+        return new_date_time
+    
+    @validates('tix_low')
+    def validates_tix_low(self, _, new_tix_low):
+        if not isinstance(bool, new_tix_low):
+            raise TypeError(
+                'tix_low must be a boolean.'
+            )
+        return new_tix_low
+    
+    @validates('sold_out')
+    def validates_sold_out(self, _, new_sold_out):
+        if not isinstance(bool, new_sold_out):
+            raise TypeError(
+                'sold_out must be a boolean.'
+            )
+        return new_sold_out
+    
+    @validates('artist_id')
+    def validate_artist_id(self, _, new_artist_id):
+        if new_artist_id == None:
+            raise ValueError(
+                'Artist ID is required.'
+                )
+        artist = db.session.get(Artist, new_artist_id)
+        if not db.session.get(Artist, new_artist_id):
+            raise ValueError(
+                'That artist does not exist in the database.'
+                )
+        self.artist = artist
+        return new_artist_id
+    
+    @validates('venue_id')
+    def validate_venue_id(self, _, new_venue_id):
+        if new_venue_id == None:
+            raise ValueError(
+                'Venue ID is required.'
+                )
+        venue = db.session.get(Venue, new_venue_id)
+        if not venue:
+            raise ValueError(
+                'That venue does not exist in the database.'
+                )
+        self.venue = venue
+        return new_venue_id
 
     def __repr__(self):
         return f'<Concert {self.id}:\nArtist:{self.artist}\nVenue:{self.venue}>'
@@ -110,6 +227,32 @@ class Review(db.Model, SerializerMixin):
         '-venue.reviews',
         '-created_at',
         '-updated_at')
+    
+    @validates('review_text')
+    def validates_review_text(self, _, new_review_text):
+        if not isinstance(str, new_review_text):
+            raise TypeError(
+                'Name must be a string.'
+            )
+        elif not len(new_review_text) in range(1, 400):
+            raise ValueError(
+                'Name must be between 1 and 400 characters.'
+            )
+        return new_review_text
+    
+    @validates('venue_id')
+    def validate_venue_id(self, _, new_venue_id):
+        if new_venue_id == None:
+            raise ValueError(
+                'Venue ID is required.'
+                )
+        venue = db.session.get(Venue, new_venue_id)
+        if not venue:
+            raise ValueError(
+                'That venue does not exist in the database.'
+                )
+        self.venue = venue
+        return new_venue_id
 
     def __repr__(self):
         return f'<Review {self.id}>'
