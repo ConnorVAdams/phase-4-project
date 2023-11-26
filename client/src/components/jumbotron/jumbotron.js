e41c5279d3f4e20ba4aefe3694b1c1663946b68d
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-11-26 13:01:28 -0500

    add text to jumbotron

--- client/src/components/jumbotron/jumbotron.js
+++ client/src/components/jumbotron/jumbotron.js
@@ -4,12 +4,12 @@ import './jumbotron.css'
 
 const Jumbotron = () => {
     return (
-        <div className="jumbotron jumbotron-fluid text-center py-5 bg-light text-dark d-flex flex-column justify-content-center">
+        <div className="jumbotron jumbotron-fluid text-center py-5 bg-light text-light text-shadow d-flex flex-column justify-content-center">
             <div className="container">
                 <h1 className="display-4">StageFinder</h1>
                 <p className="lead my-4">Find the best stages for your performances.</p>
                 <Link to="/concerts">
-                    <Button className="mt-4" variant="warning" size="lg">Explore Concerts</Button>
+                    <Button className="mt-4" variant="primary" size="lg">Explore Concerts</Button>
                 </Link>
             </div>
         </div>
