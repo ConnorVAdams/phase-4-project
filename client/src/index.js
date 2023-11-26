a4b1d728d054dce0b9bc788ea755164afd770ff1
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-11-26 11:06:05 -0500

    initial front end setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import App from "./components/App";
-import "./index.css";
-import { createRoot } from "react-dom/client";
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import routes from './routes'
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+let router = createBrowserRouter(routes)
+
+const root = ReactDOM.createRoot(document.querySelector("#root"))
+
+root.render(<RouterProvider router={router} />)
---
+++ client/src/routes.js
@@ -0,0 +1,13 @@
+import App from './app'
+
+const routes = [
+    {
+        path: '/',
+        element: <App />,
+        children: [
+
+        ]
+    }
+]
+
+export default routes
