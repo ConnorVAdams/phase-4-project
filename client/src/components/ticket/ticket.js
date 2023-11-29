87c12036d6beda85a4e6b26976fb06e87fcc3460
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-11-29 18:00:11 -0500

    add view ticket modal

--- client/src/components/ticket/ticket.js
+++ client/src/components/ticket/ticket.js
@@ -1,5 +1,7 @@
 import { Card, Button } from 'react-bootstrap';
 import { useOutletContext } from 'react-router-dom';
+import { useState } from 'react'
+import ViewTicketModal from '../viewTicketModal/viewTicketModal';
 
 const Ticket = ({ ticket }) => {
 
@@ -7,21 +9,31 @@ const Ticket = ({ ticket }) => {
 
   const { concert } = ticket
 
+  const [showModal, setShowModal] = useState(false); // State to control modal visibility
+
+  const handleShowModal = () => setShowModal(true); // Function to show the modal
+  const showTicketModal = () => handleShowModal()
+  const handleCloseModal = () => setShowModal(false); // Function to hide the modal
+
   console.log(ticket)
 
   return (
     concert ? (
-      <Card className="text-center my-3">
-        <Card.Header>{concert.date}</Card.Header>
-        <Card.Body>
-          <Card.Title>{concert.artist.name}</Card.Title>
-          <Card.Text>
-            {concert.venue.name} | {concert.venue.location} | {concert.time}
-          </Card.Text>
-          <Button onClick={() => removeFromUserTickets(ticket.id)} className="mx-2" variant="outline-danger">Sell Ticket</Button>
-          <Button className="mx-2" variant="warning" disabled>Transfer</Button>
-        </Card.Body>
-      </Card>
+      <>
+        <ViewTicketModal show={showModal} handleClose={handleCloseModal} />
+        <Card className="text-center my-3">
+          <Card.Header>{concert.date}</Card.Header>
+          <Card.Body>
+            <Card.Title>{concert.artist.name}</Card.Title>
+            <Card.Text>
+              {concert.venue.name} | {concert.venue.location} | {concert.time}
+            </Card.Text>
+            <Button onClick={() => removeFromUserTickets(ticket.id)} className="mx-2" variant="outline-danger">Sell Ticket</Button>
+            <Button onClick={showTicketModal} variant="primary">View Ticket</Button>
+            <Button className="mx-2" variant="warning" disabled>Transfer</Button>
+          </Card.Body>
+        </Card>
+      </>
     ) : " "
   );
 }
