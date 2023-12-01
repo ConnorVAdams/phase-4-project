d17f598a86d6ca33d0b93c2ce1c7400b666606ba
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-11-30 23:46:14 -0500

    sort userTickets by date functionality added

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -7,31 +7,36 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 const App = () => {
-
-  const [ userTickets, setUserTickets ] = useState([])
+  const [userTickets, setUserTickets] = useState([]);
 
   const addToUserTickets = (concert) => {
-
     const ticket = {
       id: uuid(),
       concert
-    }
+    };
 
-    setUserTickets([...userTickets, ticket])
-  }
+    // Add the new ticket and then sort the array by concert date
+    const updatedTickets = [...userTickets, ticket].sort((a, b) => {
+      const dateA = new Date(a.concert.date);
+      const dateB = new Date(b.concert.date);
+      return dateA - dateB;
+    });
+
+    setUserTickets(updatedTickets);
+  };
 
   const removeFromUserTickets = (id) => {
-    const newTickets = userTickets.filter(concert => concert.id !== id)
-    setUserTickets(newTickets)
-  }
+    const newTickets = userTickets.filter(concert => concert.id !== id);
+    setUserTickets(newTickets);
+  };
 
   return (
     <>
       <Navbar />
-      <Outlet context={{ userTickets, addToUserTickets, removeFromUserTickets }}/>
+      <Outlet context={{ userTickets, addToUserTickets, removeFromUserTickets }} />
       <Footer />
     </>
-  )
-}
+  );
+};
 
 export default App;
