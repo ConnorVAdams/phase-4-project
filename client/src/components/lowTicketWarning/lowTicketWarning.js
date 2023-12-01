da7f9266d5d9529e2ab58870c48317083217545e
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-12-01 00:11:05 -0500

    adjust low ticket variant

--- client/src/components/lowTicketWarning/lowTicketWarning.js
+++ client/src/components/lowTicketWarning/lowTicketWarning.js
@@ -2,7 +2,7 @@ import Alert from 'react-bootstrap/Alert';
 
 const LowTicketWarning = () => {
     return (
-        <Alert variant="warning">
+        <Alert variant="danger">
             <Alert.Heading><i className="fas fa-exclamation-triangle"></i> Low Tickets!</Alert.Heading>
         </Alert>
     );
