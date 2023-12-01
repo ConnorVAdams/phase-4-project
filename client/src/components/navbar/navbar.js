fe7f925678729b76f5fad8f42bf242fd158b888d
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-11-30 23:33:09 -0500

    added search bar to navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,23 +1,46 @@
-import { Navbar as Navigation, Nav, Container } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Navbar as Navigation, Nav, Container, Form, FormControl, Button } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import ScrollIndicator from '../scrollIndicator/scrollIndicator';
 
 const Navbar = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    // Add logic to handle search
+  };
+
   return (
     <Navigation bg="dark" variant="dark" expand="lg" sticky="top">
-      <Container to="/">
-        <NavLink className="nav-link">
+      <Container>
+        <NavLink className="nav-link" to="/">
             <Navigation.Brand>StageFinder</Navigation.Brand>
         </NavLink>
         <Navigation.Toggle />
         <Navigation.Collapse>
-          <Nav className="justify-content-end">
+          <Nav className="me-auto">
             <NavLink className="nav-link" to="/myTickets">My Tickets</NavLink>
             <NavLink className="nav-link" to="/artists">Artists</NavLink>
             <NavLink className="nav-link" to="/concerts">Concerts</NavLink>
             <NavLink className="nav-link" to="/venues">Venues</NavLink>
             <NavLink className="nav-link" to="/addConcert">Add Concert</NavLink>
           </Nav>
+          <Form className="d-flex" onSubmit={handleSearchSubmit}>
+            <FormControl
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+              value={searchTerm}
+              onChange={handleSearchChange}
+            />
+            <Button variant="outline-info" type="submit">Search</Button>
+          </Form>
         </Navigation.Collapse>
       </Container>
       <ScrollIndicator/>
--- client/src/pages/concerts/concerts.js
+++ client/src/pages/concerts/concerts.js
@@ -11,7 +11,7 @@ const Concerts = () => {
     let [city, setCity] = useState('');
     let [artist, setArtist] = useState('');
     const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage] = useState(6); // Adjust as needed
+    const [itemsPerPage] = useState(9); // Adjust as needed
 
     const { data } = useFetch(URL);
 
@@ -57,7 +57,7 @@ const Concerts = () => {
                         </Col>
                     ))}
                 </Row>
-                <Pagination size="lg" className="d-flex flex-row justify-content-center">
+                <Pagination size="lg" className="d-flex flex-row justify-content-center my-5">
                     {totalPages > 4 && (
                         <>
                             <Pagination.First onClick={() => paginate(1)} disabled={currentPage === 1} />
