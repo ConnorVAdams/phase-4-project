1b83325546b5f26524c2eabf92f509db1d0aa649
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-12-01 10:12:57 -0500

    add pagination logic for artistConcertsCard.js

--- client/src/components/artistConcertsCard/artistConcertsCard.js
+++ client/src/components/artistConcertsCard/artistConcertsCard.js
@@ -1,37 +1,45 @@
-import { Card, ListGroup, Button } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, ListGroup, Button, Pagination } from 'react-bootstrap';
 import formatDateString from '../../util/formatDate';
 import { useOutletContext } from 'react-router-dom';
 import LowTicketWarning from '../lowTicketWarning/lowTicketWarning';
 import BuyTicketModal from '../buyTicketModal/buyTicketModal';
-import { useState } from 'react'
-import sortByDate from '../../util/sortByDate'
+import sortByDate from '../../util/sortByDate';
 import triggerConfetti from '../../util/confettiEffect';
 
 const ArtistConcertsCard = ({ concerts }) => {
+    concerts = concerts.sort(sortByDate);
 
-    concerts = concerts.sort(sortByDate)
+    const { addToUserTickets } = useOutletContext();
+    const [showModal, setShowModal] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 3; // Adjust as needed
 
-    const { addToUserTickets } = useOutletContext()
-
-    const [showModal, setShowModal] = useState(false); // State to control modal visibility
-
-    const handleShowModal = () => setShowModal(true); // Function to show the modal
-    const handleCloseModal = () => setShowModal(false); // Function to hide the modal
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
 
     const addConcert = (e) => {
-        const [ concert ] = concerts.filter(concert => concert.id === Number(e.target.dataset.concert_id))
-        addToUserTickets(concert)
-        handleShowModal()
-        triggerConfetti()
-    }
+        const [concert] = concerts.filter(concert => concert.id === Number(e.target.dataset.concert_id));
+        addToUserTickets(concert);
+        handleShowModal();
+        triggerConfetti();
+    };
+
+    // Pagination logic
+    const totalPages = Math.ceil(concerts.length / itemsPerPage);
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = concerts.slice(indexOfFirstItem, indexOfLastItem);
+
+    // Change page
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <>
-        <Card className="text-center">
-            <Card.Header>Tour Schedule</Card.Header>
-            <ListGroup variant="flush">
-                {concerts ? concerts.map((concert, i) => {
-                    return (
+            <Card className="text-center">
+                <Card.Header>Tour Schedule</Card.Header>
+                <ListGroup variant="flush">
+                    {currentItems.map((concert, i) => (
                         <ListGroup.Item key={i} className="lead">
                             <b>{formatDateString(concert.date)}</b>
                             <p>
@@ -40,19 +48,25 @@ const ArtistConcertsCard = ({ concerts }) => {
                             {concert.tix_low ? <LowTicketWarning /> : ''}
                             <div className="d-grid gap-2">
                                 {concert.sold_out ? 
-                                <Button variant="dark" disabled >Sold Out</Button>
-                                :
-                                <Button onClick={addConcert} data-concert_id={concert.id} variant="primary">Buy Tickets</Button>
+                                    <Button variant="dark" disabled>Sold Out</Button>
+                                    :
+                                    <Button onClick={addConcert} data-concert_id={concert.id} variant="primary">Buy Tickets</Button>
                                 }
                             </div>
                         </ListGroup.Item>
-                    )
-                }) : ""}
-            </ListGroup>
-        </Card>
-        <BuyTicketModal show={showModal} handleClose={handleCloseModal} />
+                    ))}
+                </ListGroup>
+            </Card>
+            <Pagination size="lg" className="d-flex justify-content-center my-4">
+                {[...Array(totalPages).keys()].map(number => (
+                    <Pagination.Item key={number + 1} active={number + 1 === currentPage} onClick={() => paginate(number + 1)}>
+                        {number + 1}
+                    </Pagination.Item>
+                ))}
+            </Pagination>
+            <BuyTicketModal show={showModal} handleClose={handleCloseModal} />
         </>
     );
-}
+};
 
-export default ArtistConcertsCard
+export default ArtistConcertsCard;
